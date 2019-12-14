dcf07684a299d552acf84da4c7ffe32f75121788
luanfonsecap <luanfonsecap@icloud.com>
2019-12-14 13:09:18 -0300

    add routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 
+const SensorController = require('./controllers/SensorController');
+
+routes.get('/sensor', SensorController.index);
+
+routes.post('/sensor', SensorController.store);
+
+routes.put('/sensor/:id/update', SensorController.update);
+
+routes.delete('/sensor/:id/delete', SensorController.delete);
+
 module.exports = routes;
